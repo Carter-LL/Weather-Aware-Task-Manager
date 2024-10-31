a7d03090f9bc9cc8affcea8aa1259aea23b7df03
Carter <carter@alumni.com>
2024-10-31 13:18:21 -0400

    Create diagram.sql

---
+++ diagram.sql
@@ -0,0 +1,39 @@
++---------------------+
+|      Frontend       |
+|  (ASP.NET Core MVC) |
+|                     |
+|  - User Interface   |
+|  - Task Management  |
+|  - Weather Display  |
++---------------------+
+          |
+          |
+          v
++---------------------+
+|      Backend        |
+|  (ASP.NET Core)     |
+|                     |
+|  - Controller Layer |
+|  - Business Logic   |
+|  - API Integration  |
++---------------------+
+          |
+          |
+          v
++---------------------+
+|      Data Layer     |
+|      (SQLite)       |
+|                     |
+|  - User Data        |
+|  - Task Data        |
++---------------------+
+          |
+          |
+          v
++---------------------+
+|   Weather API       |
+|  (e.g., OpenWeatherMap) |
+|                     |
+|  - Current Weather  |
+|  - Forecast Data    |
++---------------------+
